ea51df417b6ef3cbc87dade14c39dc88b79fb299
Michael Gregory <michael.gregory8584@gmail.com>
2020-10-07 22:04:12 -0500

    test

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
         </a>
         !
       </h2>
+      <h3>Test</h3>
       <p>
         <a
           href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
